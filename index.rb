02f4eaa32271904081f01a839d2897ea95417ce5
Roman Storm <rstormsf@gmail.com>
2019-11-11 22:40:47 -0800

    fix

--- index.rb
+++ index.rb
@@ -3,6 +3,7 @@ require 'dotenv/load'
 
 include Eth::Utils
 
+
 def encode_params ticket
     result = bin_to_hex zpad_int(ticket[:plan], 1)
     result += bin_to_hex zpad_int(ticket[:price])
@@ -13,15 +14,22 @@ end
 
 ticket = {
     plan: 1,
-    price: 12000000000000000000,
-    ticketExpiration: 1573533439,
-    contractAddress: '0x0f5Ea0A652E851678Ebf77B69484bFcD31F9459B'
+    price: 1200000000000000000,
+    ticketExpiration: 1573540866,
+    contractAddress: '0x03Ebd0748Aa4D1457cF479cce56309641e0a98F5'
 }
 
 key = Eth::Key.new priv: ENV['PRIVATE_KEY']
 params = encode_params ticket
 puts "params: #{params}"
-bin = keccak256(hex_to_bin params )
-hex = bin_to_prefixed_hex bin
-puts "hash: #{hex}"
-puts "signature: 0x#{key.personal_sign(hex)}"
+msg_bin = keccak256(hex_to_bin params )
+msg_hex = bin_to_prefixed_hex msg_bin
+
+# prefix_with_msg = keccak256 prefix_message msg_bin
+# puts "prefix msg #{bin_to_prefixed_hex prefix_with_msg}"
+puts "hash: #{msg_hex}"
+puts "signature: 0x#{key.personal_sign(msg_bin)}"
+sig = key.personal_sign(msg_bin)
+puts "signer: #{key.address.downcase}"
+
+puts "recover #{public_key_to_address Eth::Key.personal_recover(msg_hex, sig)}" 
